3808ac6da2ecddc2055c974229d366ddcd874bca
Thungghuan <1092887426@qq.com>
2020-03-09 16:35:41 +0800

    完善功能

--- Web_Control_LED/LED/LED.ino
+++ Web_Control_LED/LED/LED.ino
@@ -33,9 +33,10 @@ void setup(){
 
     server.begin();
     server.on("/", HTTP_GET, handleRoot);
-    server.on("/LEDChange", HTTP_POST, LEDChange);
-    server.on("/LEDblink",  LEDblink);
-    server.on("/LEDbreathe",  LEDbreathe);
+    server.on("/LEDChange", HTTP_POST, LEDon);
+    server.on("/LEDblink", HTTP_POST, LEDblink);
+    server.on("/LEDbreathe", HTTP_POST, LEDbreathe);
+    server.on("/LEDstop", HTTP_POST, LEDstop);
     server.onNotFound(handleNotFound);
 }
 
@@ -44,14 +45,15 @@ void loop(){
 }
 
 void handleRoot(){
-    digitalWrite(LED_BUILTIN, HIGH);
     String s;
     s += "<!DOCTYPE html><html lang=\"en\"><head><meta charset=\"UTF-8\">";
-    s += "<style>body{height: 100hx;margin: 30% 50%;display: flex;align-items: center;justify-content: center;}button{margin: 20px;width: 50px;height: 30px;}</style>";
+    s += "<style>body{margin: auto 50%;display: flex;align-items: center;justify-content: center;}button{margin: 20px;width: 50px;height: 30px;}</style>";
     s += "<title>Control the LED_BUILTIN</title></head>";
-    s += "<body><form action=\"/LEDChange\" method=\"POST\"><button>开/关</button></form>";
-    s += "<form action=\"/LEDBlink\" method=\"GET\"><button>闪烁</button></form>";
-    s += "<form action=\"/LEDBreathe\" method=\"GET\"><button>呼吸</button></form></body></html>";
+    s += "<body><form action=\"/LEDChange\" method=\"POST\"><button>开</button></form>";
+    s += "<form action=\"/LEDblink\" method=\"POST\"><button>闪烁</button></form>";
+    s += "<form action=\"/LEDbreathe\" method=\"POST\"><button>呼吸</button></form>";
+    s += "<form action=\"/LEDstop\" method=\"POST\"><button>停止</button></form>";
+    s += "</body></html>";
     server.send(200, "text/html", s);
 }
 
@@ -59,40 +61,40 @@ void handleNotFound(){
     server.send(404, "text/html", "<!DOCTYPE html><html lang=\"en\"><head><meta charset=\"UTF-8\"><meta name=\"viewport\"content=\"width=device-width, initial-scale=1.0\"><title>开关灯</title></head><body><h1>404 NOT FOUND</h1></body>");
 }
 
-void LEDChange(){
-    digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+void LEDon(){
+    digitalWrite(LED_BUILTIN, LOW);
     server.sendHeader("Location", "/");
     server.send(303);
 }
 
 void LEDblink() {
-    String s;
-    s = "<!DOCTYPE html><html lang=\"en\"><head><meta charset=\"UTF-8\">";
-    s += "<title>Document</title></head>";
-    s += "<body><form action=\"/\" method=\"GET\"><button>闪烁中</button></form></body></html>"
-    server.send(s);
-    while(1) {
+    for(unsigned int i = 0;i < 5;++i) {
         digitalWrite(LED_BUILTIN, LOW);
         delay(500);
         digitalWrite(LED_BUILTIN, HIGH);
         delay(500);
     }
+    server.sendHeader("Location", "/");
+    server.send(303);
 }
 
-void handleLEDbreathe() {
-    String s;
-    s = "<!DOCTYPE html><html lang=\"en\"><head><meta charset=\"UTF-8\">";
-    s += "<title>Document</title></head>";
-    s += "<body><form action=\"/\" method=\"GET\"><button>呼吸中</button></form></body></html>"
-    server.send(s);
-    while(1) {
-        for (int i=0;i <= 1023;i++) {
-            analogWrite(LED_BUILTIN,i);
-            delay(1);
-        }
+void LEDbreathe() {
+    for(unsigned int i = 0;i < 5;++i) {
         for (int i=1023;i >= 0;i--) {
             analogWrite(LED_BUILTIN,i);
             delay(1);
         }
+        for (int i=0;i <= 1023;i++) {
+            analogWrite(LED_BUILTIN,i);
+            delay(1);
+        }
     }
+    server.sendHeader("Location", "/");
+    server.send(303);
+}
+
+void LEDstop() {
+    digitalWrite(LED_BUILTIN, HIGH);
+    server.sendHeader("Location", "/");
+    server.send(303);
 }
